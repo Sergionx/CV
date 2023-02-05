8507b9441aa0a08aba04a275c1fc6025a4d79fe7
Sergionx <sergio180203@gmail.com>
2023-02-05 16:04:55 -0400

    contacto preliminar

--- main.js
+++ main.js
@@ -55,3 +55,28 @@ skills.forEach(function(skill){
     <div class="progress-bar" role="progressbar" style="width: ${skill.percent}%; background-color: ${skill.color};" aria-valuenow="${skill.percent}" aria-valuemin="0" aria-valuemax="100">${skill.percent}% ${"   "}</div>
   `
 })
+
+function contact(){
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var message = document.getElementById('message').value;
+//   error_message.style.padding = "10px";
+//   var text;
+//   if(name.length < 5){
+//     text = "Please Enter valid Name";
+//     error_message.innerHTML = text;
+//     return false;
+//   }
+//   if(email.indexOf("@") == -1 || email.length < 6){
+//     text = "Please Enter valid Email";
+//     error_message.innerHTML = text;
+//     return false;
+//   }
+//   if(message.length <= 140){
+//     text = "Please Enter More Than 140 Characters";
+//     error_message.innerHTML = text;
+//     return false;
+//   }
+//   alert("Form Submitted Successfully!");
+//   return true;
+}
